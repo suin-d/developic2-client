39489afcd8a1d4108ba43e383a9ec51a60c7b440
suin-d <everytheone@gmail.com>
2021-06-24 21:59:24 +0900

    Add @eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
   plugins: ['prettier', '@typescript-eslint'],
   rules: {
