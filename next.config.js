5f3e262d7c1f2fdbb75f6289821cecad3f0b1279
suin-d <everytheone@gmail.com>
2021-07-30 17:18:16 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
       'developic2.s3.ap-northeast-2.amazonaws.com',
       'k.kakaocdn.net',
       'lh3.googleusercontent.com',
+      'platform-lookaside.fbsbx.com',
     ],
   },
   experimental: {
