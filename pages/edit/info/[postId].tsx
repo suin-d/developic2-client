45f801779437e5463a0b828b4a92299725e61aad
suin-d <everytheone@gmail.com>
2021-08-20 00:05:58 +0900

    Fix edit-post-info page
    - 댓글허용여부/글공개여부/라이센스정보 체크방식 useRadioButton hook을 이용해 수정
    - 요약(summary)을 변경하고 발행한 후, 다시 글 수정할 때 요약 내용에 변경한 값이 아니라 content.replace값 가져오는 오류 수정

--- pages/edit/info/[postId].tsx
+++ pages/edit/info/[postId].tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { copyRightData } from '../../../utils/data';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+import { copyRightData } from '../../../utils/data';
 import ImageDropZone from '../../../components/Input/ImageDropZone';
 import SquareBtn from '../../../components/Button/SquareBtn';
-import CustomCheckBox from '../../../components/Input/CustomCheckBox';
+import { useRadioButton } from '../../../hooks/useRadioButton';
 import PicstoryModal from '../../../components/Modal/PicstoryModal';
 import Layout from '../../../components/Layout';
 import { useRouter } from 'next/router';
@@ -49,11 +49,13 @@ export const InfoPostContainer = styled.div`
     flex: 1;
     padding-left: 50px;
     .check__group {
-      display: flex;
     }
     .license__group {
-      li {
-        margin-bottom: 7.5px;
+      ul {
+        flex-wrap: wrap;
+        li {
+          margin-bottom: 7.5px;
+        }
       }
     }
     .btn__group {
@@ -85,42 +87,95 @@ export const InfoPostContainer = styled.div`
 `;
 
 export default function InfoPost(): JSX.Element {
-  const { tempPost, submitPostDispatch, getTempPostDispatch, submitPost } = usePost();
+  const {
+    tempPost,
+    submitPostDispatch,
+    getTempPostDispatch,
+    submitPost,
+    getPostDetail,
+  } = usePost();
   const { userData } = useUser();
-
   const router = useRouter();
+
+  useEffect(() => {
+    getTempPostDispatch(router.query.postId as string);
+  }, [getTempPostDispatch, router.query]);
+
   const [picstoryOpen, setPicstoryOpen] = useState(false);
-  const [allowComment, setAllowComment] = useState(tempPost.data?.allowComment === 1);
   const [summary, onChangeSummary, setSummary] = useInput(
     tempPost.data?.summary
       ? tempPost.data?.summary
       : tempPost.data?.content.replace(/(<([^>]+)>)/gi, '').substr(0, 100)
   );
-  const [isPublic, setIsPublic] = useState(tempPost.data?.isPublic === 1);
-  const [copyRight, setCopyRight] = useState(
-    tempPost.data?.lisence ? tempPost.data.lisence : 'Copyright © All Rights Reserved'
+
+  // 댓글허용여부: 허용(1), 비허용(0)
+  const allowCommentLabel = ['허용', '비허용'];
+  const [isAllowComment, setIsAllowComment] = useState(
+    tempPost.data?.allowComment === 0 ? '비허용' : '허용'
   );
+  const [allowCommentValue, renderAllowCommentChecks] = useRadioButton(
+    allowCommentLabel,
+    'allowComment',
+    isAllowComment
+  );
+  useEffect(() => {
+    setIsAllowComment(allowCommentValue === '허용' ? '허용' : '비허용');
+  }, [isAllowComment, allowCommentValue]);
+
+  // 글공개여부: 공개(1), 비공개(0)
+  const publicLabel = ['공개', '비공개'];
+  const [isPublic, setIsPublic] = useState(
+    tempPost.data?.isPublic === 0 ? '비공개' : '공개'
+  );
+  const [publicValue, renderPublicChecks] = useRadioButton(
+    publicLabel,
+    'isPublic',
+    isPublic
+  );
+  useEffect(() => {
+    setIsPublic(publicValue === '공개' ? '공개' : '비공개');
+  }, [isPublic, publicValue]);
+
+  const [copyRight, setCopyRight] = useState(
+    tempPost.data?.license ? tempPost.data.license : 'Copyright © All Rights Reserved'
+  );
+  const [copyrightValue, renderCopyrightChecks] = useRadioButton(
+    copyRightData,
+    'copyRight',
+    copyRight
+  );
+  useEffect(() => {
+    switch (copyrightValue) {
+      case 'Copyright © All Rights Reserved':
+        setCopyRight('Copyright © All Rights Reserved');
+        break;
+      case 'CC BY (저작자표시)':
+        setCopyRight('CC BY (저작자표시)');
+        break;
+      case 'CC BY-SA (저작자표시-동일조건변경허락)':
+        setCopyRight('CC BY-SA (저작자표시-동일조건변경허락)');
+        break;
+      case 'CC BY-ND (저작자표시-변경금지)':
+        setCopyRight('CC BY-ND (저작자표시-변경금지)');
+        break;
+      case 'CC BY-NC (저작자표시-비영리)':
+        setCopyRight('CC BY-NC (저작자표시-비영리)');
+        break;
+      case 'CC BY-NC-SA (저작자표시-비영리-동일조건변경허락)':
+        setCopyRight('CC BY-NC-SA (저작자표시-비영리-동일조건변경허락)');
+        break;
+      case 'CC BY-NC-ND (저작자표시-비영리-변경금지)':
+        setCopyRight('CC BY-NC-ND (저작자표시-비영리-변경금지)');
+        break;
+    }
+  }, [copyrightValue]);
+
   const [thumbnail, setThumbnail] = useState(
     tempPost.data?.thumbnail ? tempPost.data.thumbnail : ''
   );
   const { toastOpenDispatch } = useUI();
   const goBack = () => {
     router.replace(`/edit/content/${router.query.postId}`);
-  };
-
-  const onSubmitPost = () => {
-    if (!summary.trim()) return toastOpenDispatch('요약글을 작성해주세요.');
-    if (!thumbnail.trim()) return toastOpenDispatch('썸네일을 등록해주세요.');
-
-    const data = {
-      allowComment: allowComment ? 1 : 0,
-      isPublic: isPublic ? 1 : 0,
-      thumbnail,
-      summary,
-      license: copyRight,
-      PostId: router.query.postId as string,
-    };
-    submitPostDispatch(data);
   };
 
   useEffect(() => {
@@ -131,13 +186,24 @@ export default function InfoPost(): JSX.Element {
   }, [submitPost.data, router.query]);
 
   useEffect(() => {
-    getTempPostDispatch(router.query.postId as string);
-  }, [router.query]);
-  useEffect(() => {
-    if (tempPost.data) {
-      setSummary(tempPost.data.content.replace(/(<([^>]+)>)/gi, '').substr(0, 100));
+    if (!getPostDetail.data?.summary && tempPost.data) {
+      setSummary(tempPost.data?.content.replace(/(<([^>]+)>)/gi, '').substr(0, 100));
     }
   }, [tempPost.data]);
+
+  const onSubmitPost = useCallback(() => {
+    if (!summary.trim()) return toastOpenDispatch('요약글을 작성해주세요.');
+    if (!thumbnail.trim()) return toastOpenDispatch('썸네일을 등록해주세요.');
+    const data = {
+      allowComment: isAllowComment === '허용' ? 1 : 0,
+      isPublic: isPublic === '공개' ? 1 : 0,
+      thumbnail,
+      summary,
+      license: copyRight,
+      PostId: router.query.postId as string,
+    };
+    submitPostDispatch(data);
+  }, [isAllowComment, copyRight, isPublic, router.query.postId, summary, thumbnail]);
 
   if (!tempPost.data) return <></>;
 
@@ -161,43 +227,11 @@ export default function InfoPost(): JSX.Element {
             <SquareBtn onClick={() => setPicstoryOpen(true)}>픽스토리 선택</SquareBtn>
           </div>
           <h5>댓글허용여부</h5>
-          <div className="check__group">
-            <CustomCheckBox
-              title="허용"
-              value={allowComment}
-              onChange={() => setAllowComment(true)}
-            />
-            <CustomCheckBox
-              title="비허용"
-              value={!allowComment}
-              onChange={() => setAllowComment(false)}
-            />
-          </div>
+          <div className="check__group">{renderAllowCommentChecks()}</div>
           <h5>비공개여부</h5>
-          <div className="check__group">
-            <CustomCheckBox
-              title="공개"
-              value={isPublic}
-              onChange={() => setIsPublic(true)}
-            />
-            <CustomCheckBox
-              title="비공개"
-              value={!isPublic}
-              onChange={() => setIsPublic(false)}
-            />
-          </div>
+          <div className="check__group">{renderPublicChecks()}</div>
           <h5>저작권 및 라이센스</h5>
-          <ul className="license__group">
-            {copyRightData.map((license, index) => (
-              <li key={license + index}>
-                <CustomCheckBox
-                  title={license}
-                  value={copyRight === license}
-                  onChange={() => setCopyRight(license)}
-                />
-              </li>
-            ))}
-          </ul>
+          <div className="license__group">{renderCopyrightChecks()}</div>
 
           <div className="btn__group">
             <SquareBtn onClick={goBack}>취소</SquareBtn>
