83f2238c2dd29b7bc06631b7cbead4d20055cfd7
suin-d <everytheone@gmail.com>
2022-03-24 19:00:56 +0900

    Update hooks and utills
    - update favicon
    - update calcImageSrc and getServerSidePropsTemplate
    - update useAuth

---
+++ hooks/useAuth.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router';
+import { useCallback, useEffect } from 'react';
+import { User } from '../modules/user';
+import useUser from '../modules/user/hooks';
+
+type UseAuthType = {
+  replace: boolean; //true시 홈으로
+};
+export function useAuth({ replace = false }: UseAuthType): { userData: User | null } {
+  const { userData, authDispatch, auth } = useUser();
+  const router = useRouter();
+
+  const goHome = useCallback(() => {
+    if (replace && auth.error) {
+      alert('로그인이 필요한 서비스입니다.');
+      router.replace('/');
+    }
+  }, [auth.error, replace, router]);
+
+  useEffect(() => {
+    authDispatch();
+  }, []);
+
+  useEffect(() => {
+    goHome();
+  }, [auth.error, goHome]);
+
+  return { userData };
+}
+
+export default useAuth;
--- utils/getServerSidePropsTemplate.ts
+++ utils/getServerSidePropsTemplate.ts
@@ -2,7 +2,6 @@
 import axios from 'axios';
 import { GetServerSideProps } from 'next';
 import { Store } from 'redux';
-import _forEach from 'lodash/forEach';
 import wrapper from '../modules/store';
 import { State } from '../modules';
 import { authAction } from '../modules/user';
@@ -14,38 +13,29 @@ interface ContextType extends GetServerSidePropsContext {
 }
 
 type GetServerSidePropsContextType = (
-  callBackArr?: Array<any>
+  callBackArr: Array<any>
 ) => GetServerSideProps<Promise<void>, ParsedUrlQuery>;
 
-const initialGetServerSideProps: GetServerSidePropsContextType = callBackArr => {
+export const getMultipleServerSideProps: GetServerSidePropsContextType = callBackArr => {
   const getServerSideProps = wrapper.getServerSideProps(async context => {
-    console.log('SSR시작');
-    //쿠키 전달
-    const cookie = context.req ? context.req.headers.cookie : ''; //이 안에 쿠키 들어있음.
-    //쿠키 공유 방지
-    axios.defaults.headers.Cookie = ''; //아닐때는 쿠키 제거
+    const cookie = context.req ? context.req.headers.cookie : '';
+    axios.defaults.headers.Cookie = '';
     if (context.req && cookie) {
-      //서버일때 & 쿠키가 있을때만
-      axios.defaults.headers.Cookie = cookie; //쿠키를 넣어주고
+      axios.defaults.headers.Cookie = cookie;
     }
-    await context.store.dispatch(authAction(null));
-    _forEach(callBackArr, async callback => await context.store.dispatch(callback));
-    console.log('SSR끝');
+    await Promise.all([
+      context.store.dispatch(authAction(null)),
+      callBackArr.map(callback => context.store.dispatch(callback)),
+    ]);
   });
   return getServerSideProps;
 };
 
 export const authServersiceAction = async (context: ContextType): Promise<void> => {
-  console.log('SSR시작');
-  //쿠키 전달
-  const cookie = context.req ? context.req.headers.cookie : ''; //이 안에 쿠키 들어있음.
-  //쿠키 공유 방지
-  axios.defaults.headers.Cookie = ''; //아닐때는 쿠키 제거
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
   if (context.req && cookie) {
-    //서버일때 & 쿠키가 있을때만
-    axios.defaults.headers.Cookie = cookie; //쿠키를 넣어주고
+    axios.defaults.headers.Cookie = cookie;
   }
   await context.store.dispatch(authAction(null));
 };
-
-export default initialGetServerSideProps;
